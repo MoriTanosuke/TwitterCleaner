890607dc6bdd7b121b93cba7f2681179134bce3b
Carsten Ringe <carsten.ringe@web.de>
2010-09-26 12:02:25 +0200

    Added simple servlets to display timeline and destroy statuses.

---
+++ src/de/kopis/twittercleaner/DeleteTweetServlet.java
@@ -0,0 +1,32 @@
+package de.kopis.twittercleaner;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+public class DeleteTweetServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
+		final Twitter twitter = (Twitter) req.getSession().getAttribute("twitter");
+		try {
+			final ResponseList<Status> timeline = twitter.getUserTimeline();
+			// loop over timeline until no more tweets are available
+//			while (timeline != null && timeline.size() > 0) {
+				for (final Status status : timeline) {
+					final long id = status.getId();
+//					twitter.destroyStatus(id);
+				}
+//			}
+		} catch (final TwitterException e) {
+			resp.getWriter()
+				.append("Error while destroying statuses: ")
+				.append(e.getMessage());
+		}
+		resp.sendRedirect("/timeline.jsp");
+	}
+}
--- src/de/kopis/twittercleaner/TwitterCleanerServlet.java
+++ src/de/kopis/twittercleaner/TwitterCleanerServlet.java
@@ -3,10 +3,14 @@ package de.kopis.twittercleaner;
 import java.io.IOException;
 import javax.servlet.http.*;
 
-@SuppressWarnings("serial")
 public class TwitterCleanerServlet extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+	private static final long serialVersionUID = 1L;
+
+	public void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
+		String redirect = "/error.jsp";
+		if(req.getSession().getAttribute("twitter") != null) {
+			redirect = "/timeline.jsp";
+		}
+		resp.sendRedirect(redirect);
 	}
 }
